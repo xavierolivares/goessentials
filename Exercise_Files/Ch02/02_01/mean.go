72fe6691e812c1da3eeac79ef9d196b30136d11f
Xavier Olivares <xm.olivares@gmail.com>
2019-09-13 16:52:59 -0400

    learned about shorter variable assignment syntax

--- Exercise_Files/Ch02/02_01/mean.go
+++ Exercise_Files/Ch02/02_01/mean.go
@@ -6,16 +6,12 @@ import (
 )
 
 func main() {
-	var x int
-	var y int
 
-	x = 1
-	y = 2
+	x, y := 1.0, 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean int
-	mean := (x + y) / 2
+	mean := (x + y) / 2.0
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
