e609c8ea40c466c86ad1e00b4090a4eb8b5f7220
Xavier Olivares <xm.olivares@gmail.com>
2019-09-14 15:30:04 -0400

    learning about if statement syntax

--- Exercise_Files/Ch02/02_02/if.go
+++ Exercise_Files/Ch02/02_02/if.go
@@ -6,9 +6,26 @@ import (
 )
 
 func main() {
-	x := 10
+	x := 14
 
-	if x > 5 {
+	if x > 100 {
 		fmt.Println("x is big")
+	} else {
+		fmt.Println("x is not so big")
+	}
+
+	if x > 5 && x < 15 {
+		fmt.Println("x is just right")
+	}
+
+	if x < 20 || x > 30 {
+		fmt.Println("x is out of range")
+	}
+
+	a := 11.0
+	b := 20.0
+
+	if frac := a / b; frac > 0.5 {
+		fmt.Println("a more than half of b")
 	}
 }
