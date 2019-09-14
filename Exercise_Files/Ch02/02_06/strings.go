ce34abfee4d1a578b180c1fda513540610826bb3
Xavier Olivares <xm.olivares@gmail.com>
2019-09-14 17:05:03 -0400

    learned about slicing, concatenating, and running multiline strings

--- Exercise_Files/Ch02/02_06/strings.go
+++ Exercise_Files/Ch02/02_06/strings.go
@@ -12,5 +12,34 @@ func main() {
 	fmt.Println(len(book))
 
 	fmt.Printf("book[0] = %v (type %T)\n", book[0], book[0])
-	// uint8 is a byte
+	// unit8 is a byte
+
+	// Strings in Go are immutable
+	// book[0] = 116;
+
+	// Slice (start, end), 0 base, 1/2 empty range
+	fmt.Println(book[4:11])
+
+	// Slice (no end)
+	fmt.Println(book[4:])
+
+	// Slice (no start)
+	fmt.Println(book[:4])
+
+	// Use + to concatenate strings
+	fmt.Println("t" + book[1:])
+
+	// Unicode
+	fmt.Println("It was 1/2 price!")
+
+	// Multi line
+	poem := `
+	...
+	Twenty years from now you will be more disappointed in the things you didnt do rather than the things you did do
+	So throw off the bowlings, sail away from the safe harbor, catch the trade winds in your sails
+	Explore. Dream. Discover.
+	...
+	`
+	fmt.Println(poem)
+
 }
